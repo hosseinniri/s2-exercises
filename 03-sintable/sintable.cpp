fcfd1cee8440b4943b73cda9430d9d766a7328dc
hosseinniri <dehghan@kcrnt.com>
2022-08-23 13:43:49 +0430

    error handling added

--- 03-sintable/sintable.cpp
+++ 03-sintable/sintable.cpp
@@ -2,17 +2,41 @@
 #include <string>
 #include <cmath>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    double sample_number = stod(argv[1]);
-    int table_size;
-    int resulution_bit_number = stoi(argv[2]);
-    int sin_max = (pow(2,resulution_bit_number)-1)/2;
+    // if(!isdigit(argv[1][0]) || !isdigit(argv[2][0]))
+    // {
+    //     cout << "\n invalid input argumant [arg[1] and arg[2] must be number.";
+    //     return 0;
+    // }
+    if(argc < 3)
+    {
+        cout << "Invalid input :number of argumants are not enough [at least 2 inputs requierd]";
+        return 0;
+    }
+        double sample_number ;
+         int resulution_bit_number ;
+    try
+    {
+         sample_number = stod(argv[1]);
+         resulution_bit_number = stoi(argv[2]);
+    }
+    catch(const std::exception& e)
+    {
+        cout << "error was in :";
+        std::cerr << e.what() << '\n';
+        cout << "Invalid input :[arg[1] and arg[2] must be number]."<< '\n';
+        return 0;
+    }
+    
+   
 
+    int table_size;
+    int sin_max = (pow(2, resulution_bit_number) - 1) / 2;
     table_size = sample_number;
 
     int sine_table[table_size];
@@ -20,47 +44,46 @@ int main(int argc, char* argv[])
 
     bool include_cosine = false;
 
-    cout << "Abs max of sin :" << sin_max;
-    double index;
     
-        if(argc > 3 )
+
+
+    // cout << "Abs max of sin :" << sin_max;
+    double index;
+
+    
+    if (argc > 3)
+    {
+        string arg3 = argv[3];
+        if (arg3 == "c")
         {
-            if(argv[3][0] == 'c')
-        {
-            cout << "\n table including cosine";
+            cout << "table including cosine" << "\n" ;
             include_cosine = true;
         }
         else
         {
-            cout << "\n invalid input";
+            cout << "Invalid input [Enter 'c' for cosine table generating ]" << "\n" ;
             return 0;
         }
-
-        }
-        
+    }
 
 
     for (int i = 0; i < sample_number; i++)
     {
-        index =i;
-        //std :: cout << "\n angle :" << (i/sample_number);
-        
-        
-        sine_table[i]=sin_max * sin ((index/sample_number)*(2*M_PI));
-        cout << "\n >>" << left << setw(40)<< sine_table[i] << setbase(10);
-        if(include_cosine)
+        index = i;
+        // std :: cout << "\n angle :" << (i/sample_number);
+
+        sine_table[i] = sin_max * sin((index / sample_number) * (2 * M_PI));
+        cout << ">>" << left << setw(40) << sine_table[i] << setbase(10) ;
+        if (include_cosine)
         {
-            cosine_table[i]=sin_max * cos ((index/sample_number)*(2*M_PI));
-            cout << " | " << cosine_table[i] ;
-            
+            cosine_table[i] = sin_max * cos((index / sample_number) * (2 * M_PI));
+            cout << " | " << cosine_table[i] << "\n";
         }
-        
-
+        else
+        {
+            cout << "\n";
+        }
     }
-    
 
-   
-   
-    
-   return 0;    
+    return 0;
 }
