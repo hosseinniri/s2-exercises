afad63b90fb2e15f97a451c7cf54626cea1b9066
hosseinniri <dehghan@kcrnt.com>
2022-09-19 12:02:36 +0430

    release commit

--- 07-text_bar_chart/textbarchart.cpp
+++ 07-text_bar_chart/textbarchart.cpp
@@ -6,74 +6,56 @@
 int main(int argc, char *argv[])
 {
     std::vector<int> barValue;
-    int max{0};
-    int min{0};
-    int max_index{0};
-    int min_index{0};
     for (int i = 1; i < argc; i++)
     {
         std::string argvalue{argv[i]};
         int intvalue{std::stoi(argvalue)};
         barValue.push_back(intvalue);
-        std ::cout << intvalue << "\n";
-        if (intvalue < min)
-        {
-            min = intvalue;
-            min_index = i;
-        }
-        if (intvalue > max)
-        {
-            max = intvalue;
-            max_index = i;
-        }
     }
 
-    std::cout << "min" << min << "\n";
-    std::cout << "max" << max << "\n";
+    int max{*std::max_element(barValue.begin(), barValue.end())};
+    int min{*std::min_element(barValue.begin(), barValue.end())};
 
     int y_range{max - min + 1};
     int x_range{argc};
 
-
-    std::vector<int> v = barValue;
-    std::sort(v.begin(), v.end(), std::greater<int>());
-    int z=0;
-
-    std::cout <<"^\n";
-
-
-    for (int j = 0 ; j < y_range; j++)
+    std::cout << "^\n";
+    for (int j = max - 1; j >= 0; j--)
     {
-        // std::vector<int> maxidx;
-        // // std::cout<<"|";
-        
-
-        for (int i = 0; i < x_range-1; i++)
+        std::cout << "|";
+        for (int i = 0; i < x_range - 1; i++)
         {
 
-            if(barValue[i] == v[0])
+            if ((barValue[i] - (j) > 0))
             {
-                
-                // maxidx.push_back(i); 
                 std::cout << "#";
             }
-            
             else
                 std::cout << " ";
-            
         }
-        v.erase(v.begin());
         std::cout << "\n";
-        // if(j==3)
-        // {
-        //     for(int i=0; i<x_range;i++) 
-        //     if(i==0) std::cout<<"+";
-        //     else std::cout<< "-";
-        // }
-   
-        
     }
-    std::cout <<"v\n";
+    std::cout << "+";
+    for (int i = 0; i < x_range-1; i++)
+        std::cout << "#";
+    std::cout << "\n";
+
+    for (int j = 0; j > min; j--)
+    {
+        std::cout << "|";
+        for (int i = 0; i < x_range - 1; i++)
+        {
+            if ((barValue[i] - (j) < 0))
+            {
+                std::cout << "#";
+            }
+
+            else
+                std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "v";
 
     return 0;
 }
